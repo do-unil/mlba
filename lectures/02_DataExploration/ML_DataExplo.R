7c622020352cd85b344a86212f445da799606b88
unco3892 <47745523+Unco3892@users.noreply.github.com>
2025-03-02 15:14:10 +0100

    Update the script for `ML_dataExplo.R`

--- lectures/02_DataExploration/ML_DataExplo.R
+++ lectures/02_DataExploration/ML_DataExplo.R
@@ -62,6 +62,8 @@ ggplot(df, aes(x = age)) +
 ## cat*cat: bar plots
 ggplot(df, aes(x=job, fill=deposit)) + 
   geom_bar(position="dodge") + coord_flip()
+ggplot(df, aes(x=job, fill=education)) + 
+  geom_bar(position="stack") + coord_flip()
 ## cat*cat: mosaic plot
 library(ggmosaic)
 ggplot(df) +
@@ -82,73 +84,40 @@ ggpairs(df[,c(1,6,10,12,13,17)], aes(colour = deposit, alpha = 0.4))
 
 ## cat*cat*cat: sankey diagram
 
-####
-# Code below may not work in newer versions of read.table('filename', header = TRUE,  sep = '	',  stringsAsFactors = FALSE)
-# library(ggsankey)
-# df_skey <- df %>% make_long(housing, deposit, job)
-# ggplot(df_skey, aes(x = x, next_x = next_x, 
-#                     node = node,
-#                     next_node = next_node,
-#                     fill = node,
-#                     label = node)) +
-#   geom_sankey(flow.alpha = 0.5, node.color = "black",
-#               show.legend = FALSE) + 
-#   geom_sankey_label(show.legend = FALSE)
+# Load necessary libraries
+library(ggplot2)
+library(ggalluvial)
 
-# ## To see better, we diminish the number of modalities in job
-# df$job_short <- ifelse(df$job %in% c("management","blue-collar","technician","admin.","services","retired"),
-#          as.character(df$job), "other") %>% factor()
-# df_skey <- df %>% make_long(housing, deposit, job_short)
-# ggplot(df_skey, aes(x = x, next_x = next_x, 
-#                     node = node,
-#                     next_node = next_node,
-#                     fill = node,
-#                     label = node)) +
-#   geom_sankey(flow.alpha = 0.5, node.color = "black",
-#               show.legend = FALSE) + 
-#   geom_sankey_label(show.legend = FALSE) +   theme_sankey(base_size = 16)
+# Plot Sankey diagram using ggalluvial
+df %>%
+  count(housing, deposit, job, name = "count") %>%
+ggplot(aes(axis1 = housing, axis2 = deposit, axis3 = job, y = count)) +
+  geom_alluvium(aes(fill = housing)) +
+  geom_stratum() +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  scale_x_discrete(limits = c("Housing", "Deposit", "Job")) +
+  theme_minimal() +
+  labs(title = "Sankey Diagram using ggalluvial")
 
-# ## only num: parallel coordinates
-# ggparcoord(df,
-#            columns = c(1,6,10,12,13), groupColumn = 17, 
-#            scale="center",
-#            showPoints = TRUE, 
-#            order="anyClass")
-####
+## Rename job label with category "other"
+job_counts <- df %>%
+  count(job, name = "count") %>%
+  arrange(desc(count))
+total_count <- sum(job_counts$count)
+cumulative_sum <- cumsum(job_counts$count)
+job_counts <- job_counts %>%
+  mutate(cumulative_prop = cumulative_sum / total_count)
+top_jobs <- job_counts$job[job_counts$cumulative_prop <= 0.80]
+levels(df$job) <- ifelse(levels(df$job) %in% top_jobs, levels(df$job), "other")
 
-# Using ggalluvial as an alternative to ggsankey
-# library(ggalluvial)
-# library(ggplot2)
-# library(dplyr)
+df %>%
+  count(housing, deposit, job, name = "count") %>% 
+  ggplot(aes(axis1 = housing, axis2 = deposit, axis3 = job, y = count)) +
+  geom_alluvium(aes(fill = housing)) +
+  geom_stratum(reverse=FALSE) +
+  geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+  scale_x_discrete(limits = c("Housing", "Deposit", "Job")) +
+  theme_minimal() +
+  labs(title = "Sankey Diagram")
 
-# # Option 1: Original variables (housing, deposit, job)
-# # Aggregate the data to count the number of observations for each combination
-# df_alluv <- df %>% 
-#   count(housing, deposit, job)
 
-# # Create the alluvial diagram
-# ggplot(df_alluv,
-#        aes(axis1 = housing, axis2 = deposit, axis3 = job, y = n)) +
-#   geom_alluvium(aes(fill = deposit), width = 1/12) +
-#   geom_stratum(width = 1/12, fill = "grey", color = "black") +
-#   geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
-#   scale_x_discrete(limits = c("housing", "deposit", "job"),
-#                    expand = c(0.15, 0.05)) +
-#   ggtitle("Alluvial Diagram with ggalluvial")
-
-# # Option 2: With reduced job modalities (using job_short)
-# # Ensure job_short is defined similarly as in your original code:
-# df$job_short <- ifelse(df$job %in% c("management", "blue-collar", "technician", "admin.", "services", "retired"),
-#                          as.character(df$job), "other") %>% factor()
-
-# df_alluv2 <- df %>% 
-#   count(housing, deposit, job_short)
-
-# ggplot(df_alluv2,
-#        aes(axis1 = housing, axis2 = deposit, axis3 = job_short, y = n)) +
-#   geom_alluvium(aes(fill = deposit), width = 1/12) +
-#   geom_stratum(width = 1/12, fill = "grey", color = "black") +
-#   geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
-#   scale_x_discrete(limits = c("housing", "deposit", "job_short"),
-#                    expand = c(0.15, 0.05)) +
-#   ggtitle("Alluvial Diagram with job_short using ggalluvial")
