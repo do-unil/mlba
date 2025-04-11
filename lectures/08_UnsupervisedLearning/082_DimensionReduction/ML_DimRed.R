c6fee85c2a88911db3990181025bdd3db66a401f
Ilia Azizi <47745523+Unco3892@users.noreply.github.com>
2025-04-11 14:36:41 +0200

    Remove autoencoders

--- lectures/08_UnsupervisedLearning/082_DimensionReduction/ML_DimRed.R
+++ lectures/08_UnsupervisedLearning/082_DimensionReduction/ML_DimRed.R
@@ -4,9 +4,14 @@ library(dplyr)
 library(ggplot2)
 library(FactoMineR) 
 library(factoextra) 
-
-library(keras) ## for AE
+library(keras) ## for Auto-Encoder
 library(vip) ## for the variable importance (ae interpretation)
+
+install.packages("remotes")
+library(remotes)
+remotes::install_github("rstudio/tensorflow")
+library(tensorflow)
+install_tensorflow(envname = "r-tensorflow")
 
 ## load data
 data(house) 
@@ -25,78 +30,4 @@ fviz_cos2(house.pca, choice="var") ## cos2 plot; dimension 1
 fviz_pca_ind(house.pca) ## map of individuals
 
 fviz_pca_biplot(house.pca) ## biplot
-
-## ##################################
-## ##################################
-## ##################################
-## Autoencoder with house data
-data(house)
-df <- house[,-c(4,5)] 
-
-## Prepare the data
-set.seed(324) 
-x_train <- as.matrix(df) 
-
-## Define model
-model <- keras_model_sequential(input_shape=ncol(x_train)) ## input layer
-model %>% 
-  layer_dense(units=2, activation = "linear",
-              name="intermediate") %>% ## intermediate layer
-  layer_dense(units=ncol(x_train), activation = "linear") ## output layer
-
-## show the model structure
-summary(model)
-
-## Compile the model
-model %>% compile(optimizer=optimizer_adam(0.1),
-                  loss="mean_absolute_error")
-## fit the model
-model %>% fit(x_train, x_train, epoch=100, batch_size=10)
-
-## Extract the coding
-xx <- get_layer(model, name = "intermediate")
-inter_output_model <- keras_model(input = model$input, output = 
-                                    xx$output)
-embed <- inter_output_model %>% predict(x_train)
-## plot of the intermediate layers
-plot(embed, pch=20, xlab="Node 1", ylab="Node 2") 
-
-## Another model (deeper encoding)
-set.seed(718)
-model <- keras_model_sequential(input_shape=ncol(x_train))
-model %>% 
-  layer_dense(units=6, activation = "linear",
-              name="intermediate1") %>% 
-  layer_dense(units=3, activation = "linear",
-              name="intermediate2") %>%
-  layer_dense(units=4, activation = "linear")
-summary(model)
-model %>% compile(optimizer=optimizer_adam(0.1),
-                  loss="mean_absolute_error")
-
-model %>% fit(x_train, x_train, epoch=50, batch_size=30)
-ae_pred <- model %>% predict(x=x_train)
-
-## Intermediate layer is intermediate2. It has 3 nodes
-xx <- get_layer(model, name = "intermediate2")
-inter_output_model <- keras_model(input = model$input, output = 
-                                    xx$output)
-embed <- inter_output_model %>% predict(x_train)
-plot(embed, pch=20) 
-
-## ##############################
-## Analysis of the encoding link to the data
-mypred <- function(object, newdata){
-  xx <- newdata
-  predict(object, xx)[,3]
-}
-newdata <- cbind(x_train, Dim=embed[,1])
-vi(inter_output_model, method = "permute", target="Dim", 
-   metric = "rmse", pred_wrapper = mypred, train=newdata) %>% vip()
-newdata <- cbind(x_train, Dim=embed[,2])
-vi(inter_output_model, method = "permute", target="Dim", 
-   metric = "rmse", pred_wrapper = mypred, train=newdata) %>% vip()
-newdata <- cbind(x_train, Dim=embed[,3])
-vi(inter_output_model, method = "permute", target="Dim", 
-   metric = "rmse", pred_wrapper = mypred, train=newdata) %>% vip()
 
